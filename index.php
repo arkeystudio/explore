47a36a2b0a9b82db1438d709da6038b87b20e3af
arkeystudio <info@arkeystudio.nl>
2021-09-27 12:50:12 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<?php
+if($_POST[''] = 'submit')
+{
+    ?>
+    <script type="text/javascript">
+    alert("Welkom to GIT.........");
+</script>
+    <?php
+}
+?>
+<html>
+    <head>
+        <title>GIT....</title>
+    </head>
+    <body>
+        <input type="submit" value="submit" name="submit">
+    </body>
+</html>
